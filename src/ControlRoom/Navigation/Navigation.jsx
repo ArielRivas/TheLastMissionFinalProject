21cc8a44a09caf845379d3d75a0e1a5d0636de65
ArielRivas <102249863+ArielRivas@users.noreply.github.com>
2022-07-27 22:01:11 +0200

    navigation

--- src/ControlRoom/Navigation/Navigation.jsx
+++ src/ControlRoom/Navigation/Navigation.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import "./Navigation.scss";
 import { useSelector } from 'react-redux';
+import vidNav from '../../assets/navigation/navigation.mp4';
 
 const Navigation = () => {
+  
   const user = useSelector(state => state.auth.user);
   return (
     <section className='container__Navigation'>
@@ -10,8 +12,10 @@ const Navigation = () => {
       <h3>Navigation log</h3>
       <h3>Welcome <span className="nameNavigation">{user.userName}</span></h3>
         <p>Here you can check your navigation system</p>
+        <video className='container__Navigation-card' autoPlay muted> <source src={vidNav} type='video/mp4'></source>
+      Your browser does not support the video tag.</video>
       </div>
-      <div className='container__Navigation-card'></div>
+    
     </section>
   )
 }
