e4a52ac0c3306f089755dd01b8e8d181c44b4cef
ArielRivas <102249863+ArielRivas@users.noreply.github.com>
2022-07-27 21:21:36 +0200

    adding navigation

--- src/ChooseThePlanet/SliderCont.jsx
+++ src/ChooseThePlanet/SliderCont.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 
 
 const SliderCont = ({activeIndex, sliderImages}) => {
-
-
   return (
     <section>
       {sliderImages.map((slide, index) => (
--- src/ControlRoom/Navigation/Navigation.jsx
+++ src/ControlRoom/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import "./Navigation.scss"
+import "./Navigation.scss";
+import { useSelector } from 'react-redux';
 
 const Navigation = () => {
+  const user = useSelector(state => state.auth.user);
   return (
     <section className='container__Navigation'>
       <div className='container__Navigation-Log'>
-        <h3>Navigation log</h3>
+      <h3>Navigation log</h3>
+      <h3>Welcome <span className="nameNavigation">{user.userName}</span></h3>
         <p>Here you can check your navigation system</p>
       </div>
       <div className='container__Navigation-card'></div>
